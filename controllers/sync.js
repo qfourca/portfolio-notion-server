43cbf5d377659d2cff52aa3e16faa6bfb7863910
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-05-22 15:07:02 +0900

    [FEAT] response my updates

--- controllers/sync.js
+++ controllers/sync.js
@@ -22,22 +22,24 @@ const syncProject = async (id, updatedAt, info) => {
           },
         }
       );
+      return 1;
     }
+    return 0;
   } else {
     await Project.create({
       ...info,
       uuid: id,
       updatedAt,
     });
+    return 2;
   }
-  return;
 };
 
 exports.syncProjects = async (req, res) => {
   try {
     const projects = await getProjects();
 
-    await Promise.all(
+    const results = await Promise.all(
       projects.map((project) => {
         return (() => {
           const title = project.title[project.title.type][0];
@@ -52,7 +54,11 @@ exports.syncProjects = async (req, res) => {
       })
     );
 
-    return res.status(200).json();
+    return res.status(200).json({
+      update: results.filter((v) => v === 1).length,
+      create: results.filter((v) => v === 2).length,
+      nothing: results.filter((v) => v === 0).length,
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
